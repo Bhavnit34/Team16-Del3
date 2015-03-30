0d7a178f4795d1f29a88cbaa5be33bbd02d42f0d
Bhavnit34 <bhavnitrulez291@hotmail.com>
2015-03-30 13:40:55 +0100

    Started login page with working authentication

---
+++ DBFirstMVC/Controllers/MessageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DBFirstMVC.Controllers
+{
+    public class MessageController : Controller
+    {
+        //
+        // GET: /Message/
+        [ChildActionOnly]
+        public ActionResult TempMessage()
+        {
+            return PartialView();
+        }
+
+    }
+}
--- DBFirstMVC/User.cs
+++ DBFirstMVC/User.cs
@@ -11,11 +11,14 @@ namespace DBFirstMVC
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class User
     {
         public short UserID { get; set; }
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     
         public virtual Dept Dept { get; set; }
