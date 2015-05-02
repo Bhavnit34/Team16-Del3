870421047dd591f65312314340c52e2524a0ebdd
milka6 <l.turisova-13@student.lboro.ac.uk>
2015-05-02 19:19:45 +0100

    error trapping not finished

--- DBFirstMVC/Controllers/AdminController.cs
+++ DBFirstMVC/Controllers/AdminController.cs
@@ -209,9 +209,11 @@ namespace DBFirstMVC.Controllers
             for (var i = 0; i < Splittedwords.Length; i = i + 2)
             {
                 string temp = Splittedwords[i];
+                var temp1 = Convert.ToInt16(Splittedwords[i + 1]);
                 bool roomExit = db.Rooms.Any(o => o.RoomName == temp);
+                bool capacity = db.Rooms.Any(o => o.RoomName == temp && o.Capacity >= temp1);
 
-                if (!roomExit)
+                if (!roomExit || !capacity)
                 {
                     list.Add(Splittedwords[i]);
                 }
@@ -568,6 +570,67 @@ namespace DBFirstMVC.Controllers
             ViewBag.BuildingCode = new SelectList(db.Buildings, "BuildingCode", "BuildingName", selected);
             ViewBag.DeptCode = new SelectList(db.Depts, "DeptCode", "DeptName", selected1);
             return View(room);
+        }
+
+
+        //Delete request
+        public ActionResult DeleteReq(int id = 0)
+        {
+            Request request = db.Requests.Find(id);
+            if (request == null)
+            {
+                return HttpNotFound();
+            }
+            return View(request);
+        }
+
+        //
+        // POST: /Request/Delete/5
+
+        [HttpPost, ActionName("DeleteReq")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            //Delete any associated room requests
+            var ReqToRooms = (from d in db.RequestToRooms
+                              where d.RequestID == id
+                              select d).ToList();
+
+            for (var i = 0; i < ReqToRooms.Count; i++)
+            {
+                //take each roomRequestID
+                var rID = ReqToRooms[i].RoomRequestID;
+                //find the row in the RequestToRoom table and delete it
+                RequestToRoom row = db.RequestToRooms.Where(a => a.RoomRequestID.Equals(rID)).FirstOrDefault();
+                db.RequestToRooms.Remove(row);
+                db.SaveChanges();
+                //find the row in the RooomRequest table and delete it
+                RoomRequest RoomRow = db.RoomRequests.Where(a => a.RoomRequestID.Equals(rID)).FirstOrDefault();
+                db.RoomRequests.Remove(RoomRow);
+                db.SaveChanges();
+            }
+
+            //Delete any associated facility requests
+            var FacilityRequests = (from d in db.FacilityRequests
+                                    where d.RequestID.Equals(id)
+                                    select d).ToList();
+
+            for (var i = 0; i < FacilityRequests.Count; i++)
+            {
+                //find id of the row with the given requestID
+                var fID = FacilityRequests[i].FacilityRequestID;
+                FacilityRequest fac = db.FacilityRequests.Find(fID);
+                //delete the row
+                db.FacilityRequests.Remove(fac);
+                db.SaveChanges();
+            }
+
+
+            //Remove the Request row
+            Request request = db.Requests.Find(id);
+            db.Requests.Remove(request);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         //update changes into room table
