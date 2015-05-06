8babddcd0ed447838337084944c9d0b18cf07014
Bhavnit Patel <cobp@W7-N004-012.lunet.lboro.ac.uk>
2015-05-06 12:53:32 +0100

    Basic page with course or degree options

---
+++ DBFirstMVC/Controllers/TimetableController.cs
@@ -0,0 +1,31 @@
+﻿using DBFirstMVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace DBFirstMVC.Controllers
+{
+    public class TimetableController : BaseController    {
+        private team16Entities db = new team16Entities();
+        //
+        // GET: /Timetable/
+
+        public ActionResult Index()
+        {
+            var l = db.Lecturers.ToList();
+            var d = db.Degrees.ToList();
+
+            ViewBag.Lecturers = db.Lecturers.ToList();
+            ViewBag.Degrees = db.Degrees.ToList();
+            return View(new TimetableModel() {Degree = d, Lecturer = l });
+        }
+
+    }
+}
